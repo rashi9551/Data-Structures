2c77ef5f0b722ddc630beb7cdd729ff9ecbada8e
rashi9551 <rashidroz1361@gmail.com>
2024-01-26 12:51:21 +0530

    double ended

---
+++ circularQueue.js
@@ -0,0 +1,69 @@
+class circularQue{
+    constructor(size){
+        this.item=new Array(size)
+        this.capacity=size
+        this.length=0
+        this.front=-1
+        this.rear=-1
+    }
+    isFull()
+    {
+        return this.length==0
+    }
+    isEmpty(){
+        return this.length===0
+    }
+    enqueue(value)
+    {
+        if(this.isFull)
+        {
+            this.rear=(this.rear+1) % this.capacity
+            this.item[this.rear]=value
+            this.length++
+            if(this.front==-1)
+            {
+                this.front=this.rear
+            }
+        }else{
+            console.log("it was full");
+        }
+    }
+    dequeue()
+    {
+        if(this.isEmpty()){
+            return null
+        }
+        let items=this.item[this.front]
+        this.item[this.front]=null
+        this.front=(this.front+1) % this.capacity
+        this.length--
+        if(this.isEmpty())
+        {
+            this.front=-1
+            this.rear=-1
+        }
+        console.log(items);
+    }
+    peek()
+    {
+        console.log(this.item[this.front]);
+    }
+    size()
+    {
+        console.log(this.length);
+    }
+    print(){
+        console.log(this.item);
+    }
+}
+
+const circley=new circularQue(4)
+
+circley.enqueue(7)
+circley.enqueue(6)
+circley.enqueue(5)
+circley.enqueue(3)
+circley.dequeue()
+circley.print()
+circley.peek()
+circley.size()
--- stack.js
+++ stack.js
@@ -1,50 +1,50 @@
 // stack with array 
 
-// class stack{
-//     constructor()
-//     {
-//         this.stack1=[]
-//         this.stack2=[]
+class stack{
+    constructor()
+    {
+        this.stack1=[]
+        this.stack2=[]
 
-//     }
+    }
 
-//     push(value)
-//     {
-//         this.stack1.push(value)
-// 1
-//     }
-//     pop()
-//     {
-//         this.stack1.pop()
-//     }
-//     print()
-//     {
-//         console.log("stack:",this.stack1,"queue:",this.queue);
-//     }
-//     enqueue(){
-//         while(this.stack1.length<0){
-//         this.stack2.push(this.stack1.pop())
-//         }
-//     }
-//     dequeue(){
-//         this.stack2.pop()   
-//     }
+    push(value)
+    {
+        this.stack1.push(value)
+1
+    }
+    pop()
+    {
+        this.stack1.pop()
+    }
+    print()
+    {
+        console.log("stack:",this.stack1,"queue:",this.queue);
+    }
+    enqueue(){
+        while(this.stack1.length<0){
+        this.stack2.push(this.stack1.pop())
+        }
+    }
+    dequeue(){
+        this.stack2.pop()   
+    }
 
 
-// }
-// const stacks=new stack()
+}
+const stacks=new stack()
 
-// stacks.push(8)
-// stacks.push(7)
-// stacks.push(6)
-// stacks.push(5)
-// stacks.push(4)
-// // stacks.pop()
-// stacks.print()
-// stacks.enqueue()
-// stacks.print()
-// stacks.dequeue()
-// stacks.print()
+stacks.push(8)
+stacks.push(7)
+stacks.push(6)
+stacks.push(5)
+stacks.push(4)
+// stacks.pop()
+stacks.print()
+stacks.enqueue()
+stacks.print()
+stacks.dequeue()
+stacks.print()
 
 
 // stack with LinkedList
@@ -92,50 +92,50 @@
 // console.log(stack.pop());
 
 
-class Node{
-    constructor(value)
-    {
-        this.value=value
-        this.next=null
-    }
+// class Node{
+//     constructor(value)
+//     {
+//         this.value=value
+//         this.next=null
+//     }
 
-}
+// }
 
-class stack{
-    constructor(){
-        this.top=null
-    }
+// class stack{
+//     constructor(){
+//         this.top=null
+//     }
 
-    push(value)
-    {
-        const node=new Node(value)
-        if(this.top==null)
-        {
-            this.top=node
-        }else{
-            node.next=this.top
-            this.top=node
-        }
-    }
-    pop(){
-        this.top=this.top.next
-    }
-    peek()
-    {
-        console.log(this.top.value);
-    }
+//     push(value)
+//     {
+//         const node=new Node(value)
+//         if(this.top==null)
+//         {
+//             this.top=node
+//         }else{
+//             node.next=this.top
+//             this.top=node
+//         }
+//     }
+//     pop(){
+//         this.top=this.top.next
+//     }
+//     peek()
+//     {
+//         console.log(this.top.value);
+//     }
     
     
-}
+// }
 
-const stackey=new stack()
+// const stackey=new stack()
 
-stackey.push(6)
-stackey.push(5)
-stackey.push(4)
-stackey.push(3)
-stackey.push(2)
-stackey.peek()
-stackey.pop()
-stackey.peek()
+// stackey.push(6)
+// stackey.push(5)
+// stackey.push(4)
+// stackey.push(3)
+// stackey.push(2)
+// stackey.peek()
+// stackey.pop()
+// stackey.peek()
 
